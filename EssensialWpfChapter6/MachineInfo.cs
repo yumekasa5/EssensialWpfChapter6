43b845686f3afd479609c45bc368b395221d2846
kaigta22 <kaitomx@gmail.com>
2022-05-23 22:52:53 +0900

    Add MachineInfo

---
+++ EssensialWpfChapter6/MachineInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssensialWpfChapter6
+{
+    /// <summary>
+    /// 機器の情報をビューに渡す構造体
+    /// </summary>
+    public struct MachineInfoStruct
+    {
+        ///<summary>Number</summary>
+        public int Number { get; set; }
+
+        ///<summary>Name</summary>
+        public string Name { get; set; }
+
+        ///<summary>Value</summary>
+        public ushort Value { get; set; }
+
+        ///<summary>コンストラクタ</summary>
+        public MachineInfoStruct(int number, string name, ushort value)
+        {
+            Number = number;
+            Name = name;
+            Value = value;
+
+        }
+
+    }
+
+    /// <summary>
+    /// 機器の情報をビューに渡すクラス
+    /// </summary>
+    public class MachineInfoClass
+    {
+        ///<summary>Number</summary>
+        public int Number { get; set; }
+
+        ///<summary>Name</summary>
+        public string Name { get; set; }
+
+        ///<summary>Value</summary>
+        public ushort Value { get; set; }
+
+        ///<summary>コンストラクタ</summary>
+        public MachineInfoClass(int number, string name, ushort value)
+        {
+            Number = number;
+            Name = name;
+            Value = value;
+
+        }
+
+        ///<summary>コンストラクタ</summary>
+        public MachineInfoClass()
+        {
+
+        }
+
+    }
+}
